77e18763ffa873185bd1a83f3904c446eeac2b77
melkam <melkammekonnen899@gmail.com>
2020-12-23 14:05:54 +0300

    Creating state, constructor and methods in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ItemService from './shared/mock-item-service';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.itemService = new ItemService();
+    this.onSelect = this.onSelect.bind(this);
+    this.onNewItem = this.onNewItem.bind(this);
+    this.onEditItem = this.onEditItem.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    this.onCancelEdit = this.onCancelEdit.bind(this);
+    this.onCreateItem = this.onCreateItem.bind(this);
+    this.onUpdateItem = this.onUpdateItem.bind(this);
+    this.onDeleteItem = this.onDeleteItem.bind(this);
+
+    this.state = {
+      showDetails: false,
+      editItem: false,
+      selectedItem: null,
+      newItem: null
+    }
+  }
+  
+  getItems(){
+    this.itemService.retrieveItems().then(items => {
+      this.setState({ items: items });
+    });
+  }
+
+  componentDidMount(){
+    this.getItems();
+  }
+
+
+  render(){
+    const items = this.state.items;
+    if(!items) return null;
+
+    const showDetails = this.state.showDetails;
+    const editItem = this.state.editItem;
+    const selectedItem = this.state.selectedItem;
+    const newItem = this.state.newItem;
+
+    const listItems = items.map((item) => <li key={item.link} onClick = {() => this.onSelect(item.link)}>
+      <span className="item-name">{item.name}</span> | {item.summary} </li> );
+
+    return(
+      <div className="App">
+
+          <ul className="items">
+            {listItems}
+          </ul>
+
+          <br/>
+
+          <button type="button" name="button" onClick={() => this.onNewItem()}>New Item</button>
+
+          <br/>
+
+            {newItem && <NewItem onSubmit={this.onCreateItem} onCancel={this.onCancel}></NewItem>}
+
+            {showDetails && selectedItem && <ItemDetails item={selectedItem} onEdit={this.onEditItem}  onDelete={this.onDeleteItem} ></ItemDetails>}
+
+            {editItem && selectedItem && <EditItem onSubmit={this.onUpdateItem} onCancel={this.onCancelEdit} item={selectedItem} ></EditItem>}
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
